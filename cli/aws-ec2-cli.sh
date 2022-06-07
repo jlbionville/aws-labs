25d2c645fd067674f93401de4150c21f2e74c46b
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-07 14:22:02 +0200

    script pour obtenir le sg d'une ec2

---
+++ cli/aws-ec2-cli.sh
@@ -0,0 +1,6 @@
+# Get EC2 Security Group
+
+instanceId="i-0534f4c10d6187a0c" # Enter instance ID
+awsQuery="Reservations[].Instances[].{SecurityGroups:SecurityGroups}[0]"
+aws  ec2 describe-instances --instance-ids "$instanceId" --query "$awsQuery"
+
