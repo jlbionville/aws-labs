db6e0a5c8a2514fcaad49c78f34279d29cd4992c
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-07 14:23:04 +0200

    renvoie l'id du sg

--- cli/aws-ec2-cli.sh
+++ cli/aws-ec2-cli.sh
@@ -2,5 +2,5 @@
 
 instanceId="i-0534f4c10d6187a0c" # Enter instance ID
 awsQuery="Reservations[].Instances[].{SecurityGroups:SecurityGroups}[0]"
-aws  ec2 describe-instances --instance-ids "$instanceId" --query "$awsQuery"
+aws  ec2 describe-instances --instance-ids "$instanceId" --query "$awsQuery"| jq -r  '.SecurityGroups[0].GroupId'
 
