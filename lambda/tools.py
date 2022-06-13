34dafe0f2c5aed30d64bc1c1ab91d0722f655efa
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-13 17:54:26 +0200

    fonction pour valider la value de l'instance id

--- lambda/tools.py
+++ lambda/tools.py
@@ -16,7 +16,12 @@ def stopInstance(instanceId):
     response_json=ec2.stop_instances(InstanceIds=[instanceId])
     logger.info('stopped the instance: ' + instanceId)
     return response_json
-
+def validateInstanceId(instanceId):
+    """ check the length equal to 20 
+        evol : begin with i-
+    """    
+    if len(instanceId)!=19:
+        raise Exception ("InstanceId not valid "+instanceId)
 def validateUploadFileInBucket(fileChecksum, bucketObjectKey):
     logger.info('file in bucket to check: ' + bucketObjectKey)
     logger.info('Checksum of th file in bucket to check: ' + fileChecksum)
