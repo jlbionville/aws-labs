ea58f4d4462438c4a12f16dfee6dbba0f9de63b7
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-13 16:13:04 +0200

    simplification : gestion que d'une instance EC2

--- lambda/tools.py
+++ lambda/tools.py
@@ -6,23 +6,13 @@ logger.setLevel(logging.INFO)
 region=os.environ.get('REGION')
 ec2 = boto3.client('ec2', region_name=region)
 
-def getInstancesToStop():
-    instances = ['i-02fb51f59a615784f']
-    return instances
-def getInstancesToStart():
-    instances = ['i-02fb51f59a615784f']
-    return instances
-def setInstances(instances):
-    list_instances=instances
-    for instance in list_instances:
-        logger.info ("instance à traiter "+instance)
-def startInstances():
-    list_instances=getInstancesToStart()
-    response_json=ec2.start_instances(InstanceIds=list_instances)
-    logger.info('started your instances: ' + str(list_instances))
+def startInstance(instanceId):
+    
+    response_json=ec2.start_instances(InstanceIds=[instanceId])
+    logger.info('started the instances: ' + instanceId)
     return response_json
-def stopInstances():
-    list_instances=getInstancesToStop()
-    response_json=ec2.stop_instances(InstanceIds=list_instances)
-    logger.info('stopped your instances: ' + str(list_instances))
+def stopInstances(instanceId):
+    
+    response_json=ec2.stop_instances(InstanceIds=[instanceId])
+    logger.info('stopped the instance: ' + instanceId)
     return response_json
