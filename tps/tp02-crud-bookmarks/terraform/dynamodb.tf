7634c09a2b0f4eb0287b7fde426c589a8d83b967
Jean-Louis Bionville <jlbionville@alfaco.fr>
2025-06-21 00:16:04 +0200

    refactorisation du provider

--- tps/tp02-crud-bookmarks/terraform/dynamodb.tf
+++ tps/tp02-crud-bookmarks/terraform/dynamodb.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = "eu-west-1"
-}
 
 resource "aws_dynamodb_table" "bookmarks" {
   name         = "bookmarks"
